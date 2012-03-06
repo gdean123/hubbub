99e928757abfd6ee94727f1d2b53a56583fc5b90
George Dean <gdean123@gmail.com>
2012-03-05 16:20:42 -0800

    [GD/SD] Created new 3.0 repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hubbub::Application.config.secret_token = '06181cf7cd432feb090d351a45aa3298ceaa374ba32195dbb8fcea6a733389137a03a9841be64d7f13db796b3409a944bc670d3cd5b107f5c9a6f206137f9483'
