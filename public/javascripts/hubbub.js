41eb2a01a365f20bc8ab74bb4d6f06dfbcf0bfff
Craig Hokanson <dultimaterunner@gmail.com>
2012-03-26 17:34:59 -0700

    [CH/GD] [show error on missing description] working towards html5 validation on input filed

--- public/javascripts/hubbub.js
+++ public/javascripts/hubbub.js
@@ -2,6 +2,8 @@ var createAddItemDialog = function() {
   $("#dialog").dialog({
     autoOpen: false,
     modal: true,
+    width: 510,
+    height: 220,
     buttons: [
       { text: "OK",
         class: "create",
