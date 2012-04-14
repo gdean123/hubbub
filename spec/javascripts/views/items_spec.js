1eaf6c1815f2760ea370568a292eb4176652f85e
Surbhi Dangi <surbhi.dangi@sv.cmu.edu>
2012-04-13 22:04:13 -0700

    [SD/GD] Added sinon.js to the repository. Sinon code is currently commented out in itemps_spec.js

--- spec/javascripts/views/items_spec.js
+++ spec/javascripts/views/items_spec.js
@@ -1,20 +1,27 @@
 describe("ItemView", function() {
   var hubbubApp, appView;
-
   beforeEach(function(){
-    loadFixtures("dialog-template.html", "item-template.html");
+    // this.server = sinon.fakeServer.create();
+	loadFixtures("dialog-template.html", "item-template.html");
 
     hubbubApp = HubbubApp();
     appView = new hubbubApp.AppView();
 
     hubbubApp.Items.add(new hubbubApp.Item({"description": "test"}));
   });
+  // afterEach(function() {
+  //   this.server.restore();
+  // });
 
   describe("Instantiation", function() {
     
       it("should create a list element", function() {
-        //TypeError: Cannot read property 'models' of undefined
-        expect(hubbubApp.Items.models[0].attributes.description).toEqual("test");
+		//         this.server.respondWith("GET", "/items",
+		//         		      [200, {"Content-Type": "application/json"},
+		//         		      '{"description":"Test","details":"Test Desc","id":25}']);
+		// this.server.respond();
+		expect(hubbubApp.Items.models[0].attributes.description).toEqual("test");
       });
   });
 });
+
