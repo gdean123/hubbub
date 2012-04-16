b1a6eb501fced0432cad08d4a9b4c7d3d5ba27a5
Craig Hokanson <dultimaterunner@gmail.com>
2012-04-16 15:30:10 -0700

    [CH/SD] Added in one more test on the model

---
+++ spec/javascripts/models/item_spec.js
@@ -0,0 +1,31 @@
+describe("item", function() {
+  var hubbubApp, appView;
+  beforeEach(function(){
+    hubbubApp = HubbubApp();
+    // appView = new hubbubApp.AppView();
+    
+    hubbubApp.Items.add(new hubbubApp.Item({"description": "a todo item"}));    
+  });
+
+  it("should create a new item", function() {
+    expect(hubbubApp.Items.map(function(item){
+      return item.get("description");
+    }).toString()).toEqual("a todo item");
+  });
+  
+  describe("required", function() {
+
+      // it("should not accept a blank field in description", function() {
+      //   var eventSpy = sinon.spy();
+      //   this.item.bind("error", eventSpy);
+      //   this.item.save({"description": ""});
+      //   expect(this.eventSpy.calledOnce).toBeTruthy();
+      //   expect(this.eventSpy.calledWith(
+      //     this.item, 
+      //     "is required"
+      //   )).toBeTruthy();
+      // 
+      // });
+  });
+     
+});
