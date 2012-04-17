cdeeef9ef118bd14a459248e8337affb195c00d2
Craig Hokanson <dultimaterunner@gmail.com>
2012-04-16 17:19:50 -0700

    [CH/GD] refactoring the app, moved index file into a rails structure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def index
+    respond_to do |format|
+      format.html # index.html.erb
+    end
+  end
+  
 end
