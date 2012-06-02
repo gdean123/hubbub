50e81dc7ac3b795204c74239b64bb773657a2ea4
Craig Hokanson <dultimaterunner@gmail.com>
2012-06-02 16:51:19 -0700

    [CH/GD] working on trying to figure out the invalid token issue

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
-           '223922275629.apps.googleusercontent.com', 'ITEMTOoG67piKGzmkqMFJ_Mm',
-           {:scope          => 'https://www.google.com/base/feeds/',
-            :access_type    => 'online',
+           "223922275629.apps.googleusercontent.com", "ITEMTOoG67piKGzmkqMFJ_Mm",
+           # {:scope          => 'https://www.google.com/base/feeds/',
+           {:scope          => "https://www.google.com/base/feeds/",
+            :access_type    => "online",
             :approval_prompt => "",
+            :redirect_uri => "http://localhost:3000/auth/google_oauth2/callback",
             :client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
 end
