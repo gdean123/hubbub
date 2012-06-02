6c873a50a6f4f01930692ea2310ca9773fd5880f
Craig Hokanson <dultimaterunner@gmail.com>
2012-06-02 15:54:18 -0700

    [CH/GD] added in options on omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
            '223922275629.apps.googleusercontent.com', 'ITEMTOoG67piKGzmkqMFJ_Mm',
-           {:scope          => 'http://www.google.com/base/feeds/',
+           {:scope          => 'https://www.google.com/base/feeds/',
+            :access_type    => 'online',
+            :approval_prompt => "",
             :client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
 end
