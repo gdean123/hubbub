9dfe5795e9479b3afd02a7b2741b4d79b14439f0
George Dean <gdean123@gmail.com>
2012-07-17 15:03:15 -0700

    [GD/CH] LayoutManager should create graph edges to represent parent-child relationships

--- spec/javascripts/managers/layout_manager_spec.js
+++ spec/javascripts/managers/layout_manager_spec.js
@@ -7,7 +7,7 @@ describe("Layout Manager", function() {
 	  this.item = new hubbubApp.Item({
       "description": "test",
       "parent_id": null,
-      "id": 23
+      "id": "8c81abc9-2fab-493b-8e64-5ce64011f8c1"
     });
 
 	  this.item.url = "/";
@@ -16,5 +16,18 @@ describe("Layout Manager", function() {
   it("should create graph nodes to represent new items", function() {
     hubbubApp.Items.add(this.item);
     expect(hubbubApp.GraphNodes.length).toEqual(1);
+  });
+
+  it("should create graph edges to represent parent-child relationships", function() {
+    this.childItem = new hubbubApp.Item({
+      "id": "523c8a3b-d674-48ab-abc8-f8c7c30776aa",
+      "description": "child",
+      "parent_id": this.item.get("id")
+    });
+
+    hubbubApp.Items.add(this.item);
+    hubbubApp.Items.add(this.childItem);
+
+    expect(hubbubApp.GraphEdges.length).toEqual(1);
   });
 });
